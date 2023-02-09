b336e52aeb37e67f96f2a1224ed2eb1d7971bfc9
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-09 17:52:59 +0300

    Find matching anagrams using filter.
    Assignment Completed.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(array)
+        # use filter/select to return matching anagrams
+        array.filter do |test| 
+            test.chars.sort == @word.chars.sort            
+        end
+    end  
+
+
+end
+
+apt = Anagram.new("apt")
+puts apt.match(["pat", "tap", "jose"]) 
